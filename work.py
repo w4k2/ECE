37b879c93b7152b554733440d45bd77fd01d53a1
Paweł Ksieniewicz <pawel@ksienie.com>
2016-06-17 01:25:00 +0200

    Search.

--- work.py
+++ work.py
@@ -18,10 +18,14 @@ dataset = Dataset(sys.argv[1],sys.argv[1])
 grains = [2, 5, 10]
 limits = [1, 5, 10, 50]
 radiuses = [5, 15, 30, 100]
+
 fold = 0
 dimensionss = [[1], [2], [3], [4], [1,2], [2,3], [3,4], [2,5]]
 
 dataset.setCV(fold)
+
+winning = 0
+winner = {}
 
 print "\n| %s, fold %i" % (dataset, fold)
 print "RAD GRA LIM\tACC\tBAC\tACC\tBAC\n--- --- ---\t---\t---\t---"
@@ -33,9 +37,15 @@ for radius_i in radiuses:
 				radius = radius_i / 100.
 
 				configuration = {'radius': radius, 'grain': grain, 'limit': limit, 'dimensions': dimensions}
-				eec = EEC(dataset,configuration,EECApproach.random,ExposerParticipation.theta2)
+				eec = EEC(dataset,configuration,EECApproach.random,ExposerParticipation.lone)
 				eec.predict()
 				scores = dataset.score()
+
+				if scores['accuracy'] + scores['bac'] > winning:
+					winning = scores['accuracy'] + scores['bac']
+					winner.update(configuration)
+					winner.update(scores)
+					print '\n# LEADER\n'
 
 				print "%03i %03i %03i\t%02.2f%%\t%02.2f%% %s" % \
 					(radius_i, grain, limit, \
@@ -45,3 +55,4 @@ for radius_i in radiuses:
 end = time.time()
 
 print "%.3f seconds" % (end - start)
+print winner 
