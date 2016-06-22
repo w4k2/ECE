a3c0a092fc6754d6fe812f0e2b6cb85d58b270ad
Paweł Ksieniewicz <pawel@ksienie.com>
2016-06-22 21:50:20 +0200

    Experiment script.

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     author_email='pawel.ksieniewicz@pwr.edu.pl',
     url='https://github.com/w4k2/eec',
     package_data={'': ['LICENSE']},
+    scripts=['eec/eecExperiment',],
     license=license,
     packages=find_packages(exclude=('docs'))
 )
