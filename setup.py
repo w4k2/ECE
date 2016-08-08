32197ef5cf4a0b99282aa60b9631e338b12cd90b
Paweł Ksieniewicz <pawel@ksienie.com>
2016-08-08 22:07:57 +0200

    No eceExperiment.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open('LICENSE') as f:
 
 setup(
     name='ece',
-    version='0.4',
+    version='0.4.1',
     description='Exposer Classifier Ensemble',
     long_description=readme,
     author='Paweł Ksieniewicz',
     author_email='pawel.ksieniewicz@pwr.edu.pl',
     url='https://github.com/w4k2/ece',
     package_data={'': ['LICENSE']},
-    scripts=['ece/eceExperiment',],
+#    scripts=['ece/eceExperiment',],
     license=license,
     packages=find_packages(exclude=('docs','tests','README.md','data'))
 )
